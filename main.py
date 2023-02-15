109c5d18b4de7d4b26fe1dbb3f8ceb587278c7a2
thepu <thepuhi4@gmail.com>
2023-02-15 17:07:35 +0200

    fib

--- main.py
+++ main.py
@@ -4,9 +4,19 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
+def fib(value):
+    if value <= 0:
+        return 0
+    elif value == 1:
+        return 1
+    else:
+        return value*fib(value-1)
+
+
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(fib(5))
 
 
 # Press the green button in the gutter to run the script.
